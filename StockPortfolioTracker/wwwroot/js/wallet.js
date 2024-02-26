cbe0574c0331555e0d5e1f0a74939ba71115cf47
astisk <pobierac@gmail.com>
2024-02-27 00:42:46 +0100

    Separation of .js and .css from .cshtml files.

---
+++ StockPortfolioTracker/wwwroot/js/wallet.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#currencySwitch').change(function () {
+        var isChecked = $(this).is(":checked");
+
+        if (isChecked) {
+            $('.stockValues').hide();
+            $('.stockValuesInWalletCurrency').show();
+        } else {
+            $('.stockValues').show();
+            $('.stockValuesInWalletCurrency').hide();
+        }
+    });
+
+    $('#currencySwitch').trigger('change');
+});
---
+++ StockPortfolioTracker/wwwroot/js/wallets.js
@@ -0,0 +1,8 @@
+﻿document.querySelectorAll('.delete-btn').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        var walletName = this.getAttribute('data-wallet-name');
+        if (!confirm(`Do you wish to delete wallet ${walletName}?`)) {
+            e.preventDefault();
+        }
+    });
+});
